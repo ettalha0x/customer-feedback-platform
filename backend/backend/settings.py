22e2fabca8c76c1acfa0bb40d0b495463af8d909
Noureddine Ettalhaouy <31565378+ettalha0x@users.noreply.github.com>
2024-12-24 16:55:23 +0000

    added frontend producion url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://customer-feedback-platform-swart.vercel.app/"
+    "https://customer-feedback-platform-swart.vercel.app"
 ]
 
 ROOT_URLCONF = "backend.urls"
