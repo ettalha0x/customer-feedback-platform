67d1e538a56cdeb921b7d8e03e9620a2bbd73244
Noureddine Ettalhaouy <31565378+ettalha0x@users.noreply.github.com>
2024-12-26 15:45:02 +0000

    allow requests from front end

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
 ]
 
 # CORS_ALLOW_CREDENTIALS = True
@@ -63,8 +62,8 @@ MIDDLEWARE = [
 # Allow requests from your Next.js development server
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 
